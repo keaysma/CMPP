369ca826d64f4a4a157a22685167f659edb3c10b
Jay Wilson <jwilso48@masonlive.gmu.edu>
2019-02-02 16:20:50 -0500

    initial progress

---
+++ src/Viewer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Viewer extends Component {
+  render() {
+    return (
+      <div className="Viewer">
+        <h1>yeet</h1>
+      </div>
+    );
+  }
+};
+
+export default Viewer;
