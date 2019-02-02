f71a2ddacf79dc474add4e236b197676af92e4f6
Jacob Janz <jacobjanzcs@gmail.com>
2019-02-02 15:56:41 -0500

    Add P5.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,23 +7,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <P5Wrapper sketch={sketch} />
       </div>
     );
   }
 }
+
+export function sketch (p) {
+  let rotation = 0;
+
+  p.setup = function () {
+    p.createCanvas(1000, 1000, p.WEBGL);
+  };
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    if (props.rotation){
+      rotation = props.rotation * Math.PI / 180;
+    }
+  };
+
+  p.draw = function () {
+    p.background(100);
+    p.noStroke();
+    p.push();
+    p.rotateY(rotation);
+    p.box(100);
+    p.pop();
+  };
+};
 
 export default App;
