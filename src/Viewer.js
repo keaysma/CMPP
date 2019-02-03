bb737a62a1a5e04cbd42bd14cfc91f92e1c5b173
Jacob Janz <jacobjanzcs@gmail.com>
2019-02-02 23:09:41 -0500

    Update Viewer.js

--- src/Viewer.js
+++ src/Viewer.js
@@ -37,36 +37,8 @@ export function sketch (p) {
     }
   };*/
 
-  p.windowResized = function () {
-    resizeCanvas(windowWidth, windowHeight);
-  }
-
   p.draw = function () {
     console.log(video.get(40,20));
-  };
-};
-
-export function sketch (p) {
-  let rotation = 0;
-
-  p.setup = function () {
-    p.createCanvas(1000, 1000, p.WEBGL);
-    p.createCapture(p5.VIDEO);
-  };
-
-  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
-    if (props.rotation){
-      rotation = props.rotation * Math.PI / 180;
-    }
-  };
-
-  p.draw = function () {
-    p.background(100);
-    p.noStroke();
-    p.push();
-    p.rotateY(rotation);
-    p.box(100);
-    p.pop();
   };
 };
 
