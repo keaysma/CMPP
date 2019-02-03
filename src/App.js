9f9dc2243557383bc164c59014c8eb14243c8175
Jacob Janz <jacobjanzcs@gmail.com>
2019-02-02 19:16:33 -0500

    Fix import p5.dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import * as p5 from 'p5';
-import "p5/lib/addons/p5.dom";
+import "react-p5-wrapper/node_modules/p5/lib/addons/p5.dom.js";
 import logo from './logo.svg';
 import './App.css';
 
