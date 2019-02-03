1aca0e88a02527fec7005b50d30e2ce671878424
keaysma <michaelandrewkeays@gmail.com>
2019-02-03 01:07:50 -0500

    Some frequency modulation

--- src/App.js
+++ src/App.js
@@ -18,9 +18,24 @@ class App extends Component {
 	this.state = {
 		x: 0,
 		y: 0,
-		x2: 150,
-	    y2: 150
+		x2: 200,
+	    y2: 200
 	};
+
+	//Frequency modulation
+	/*this.car = jukebox();
+	this.car.setType('square');
+	this.car.amp(0);
+	this.car.freq(220);
+	this.car.start();
+	this.mod = jukebox();
+	this.mod.setType('sawtooth');
+	this.mod.freq(10);
+	this.mod.amp(200);
+	this.mod.start();
+	this.mod.disconnect();
+	this.car.freq(this.mod);
+	this.car.amp(0.5);*/
 
 	this.o = jukebox();
 	this.o2 = jukebox();
@@ -36,11 +51,15 @@ class App extends Component {
   updatePixels(){
     if(cam){
 	  let p = cam.get(this.state.x,this.state.y,this.state.x+1,this.state.y+1);
-	  let x = 440 + (606.5*((p[0] + p[1] + p[2])/(3*255)));
+	  let x = 98.00 + (294.00*((p[0] + p[1] + p[2])/(3*255)));
 	  let p2 = cam.get(this.state.x2,this.state.y2,1,1);
-	  let x2 = 440 + (606.5*((p2[0] + p2[1] + p2[2])/(3*255)));
+	  let x2 = 130.81 + (130.82*((p2[0] + p2[1] + p2[2])/(3*255)));
 	  this.setState((state) => {return {point: x}})
 	  console.log(p2);
+	  //For fancy modulation
+	  //this.mod.setType(((p[0] + p[1] + p[2])/(3*255)) > 0.5 ? 'sine' : 'sawtooth');
+	  ////this.mod.freq(10*((p[0]+p[1]+p[2])/(3*255)));
+	  //this.car.freq(x2);
 	  this.o.freq(x);
 	  this.o2.freq(x2);
 	}
